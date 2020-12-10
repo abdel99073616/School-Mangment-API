2dc232802974f42b24394061b323f11beac65b30
Abdo Mohamed <abdel99073616@gmail.com>
2020-12-10 23:36:02 +0200

    3

--- api/urls.py
+++ api/urls.py
@@ -3,12 +3,40 @@ from django.urls import path , include
 from .views import(
     ParentListAPIVeiw,
     ParentRetrieveAPIView ,
-    
+    ActivitesListAPIVeiw ,
+    ActivitesRetrieveAPIView ,
+    ClassroomListAPIVeiw ,
+    ClassroomRetrieveAPIView ,
+    CoursesListAPIVeiw ,
+    CoursesRetrieveAPIView ,
+    StudentListAPIVeiw ,
+    StudentRetrieveAPIView ,
+    TeacherListAPIVeiw ,
+    TeacherRetrieveAPIView
 )
 
 
 
 urlpatterns = [
     path('Parent/' , ParentListAPIVeiw.as_view() ) ,
-    path('Parent/<pk>' , ParentRetrieveAPIView.as_view() )
+    path('Parent/<pk>' , ParentRetrieveAPIView.as_view() ) ,
+    
+    path('Acticites/' , ActivitesListAPIVeiw.as_view() ) ,
+    path('Acticites/<pk>' , ActivitesRetrieveAPIView.as_view() ) ,
+    
+    path('Classroom/' , ClassroomListAPIVeiw.as_view() ) ,
+    path('Classroom/<pk>' , ClassroomRetrieveAPIView.as_view() ) ,
+    
+    path('Courses/' , CoursesListAPIVeiw.as_view() ) ,
+    path('Courses/<pk>' , CoursesRetrieveAPIView.as_view() ) ,
+    
+    path('Courses/' , CoursesListAPIVeiw.as_view() ) ,
+    path('Courses/<pk>' , CoursesRetrieveAPIView.as_view() ) ,
+    
+    path('Student/' , StudentListAPIVeiw.as_view() ) ,
+    path('Student/<pk>' , StudentRetrieveAPIView.as_view() ) ,
+    
+    path('Teacher/' , TeacherListAPIVeiw.as_view() ) ,
+    path('Teacher/<pk>' , TeacherRetrieveAPIView.as_view() ) ,
+    
 ]
