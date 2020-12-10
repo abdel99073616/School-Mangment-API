df2cbaf144706716f5fc4a3ba9a7ab38ac90daf2
Abdo Mohamed <abdel99073616@gmail.com>
2020-12-10 23:30:30 +0200

    2

--- api/serializers.py
+++ api/serializers.py
@@ -4,12 +4,9 @@ from rest_framework.serializers import ModelSerializer
 from A.models import(
     Parent_A ,
     Activites_A ,
-    Activites_student_teacher_A ,
     Course_A ,
     Classroom_A ,
     Student_A ,
-    Student_Course_A ,
-    Sudent_Teacher_A ,
     Teacher_A
 )
 
@@ -18,4 +15,39 @@ from A.models import(
 class ParentSerializer(ModelSerializer):
     class Meta :
         model = Parent_A
+        fields = '__all__'
+
+
+
+class ActivitesSerializer(ModelSerializer):
+    class Meta :
+        model = Activites_A
+        fields = '__all__'
+
+
+
+class CourseSerializer(ModelSerializer):
+    class Meta :
+        model = Classroom_A
+        fields = '__all__'
+
+
+
+class ClassroomSerializer(ModelSerializer):
+    class Meta :
+        model = Student_A
+        fields = '__all__'
+
+
+
+class StudentSerializer(ModelSerializer):
+    class Meta :
+        model = Teacher_A
+        fields = '__all__'
+
+
+
+class TeacherSerializer(ModelSerializer):
+    class Meta :
+        model = Course_A
         fields = '__all__'
--- api/views.py
+++ api/views.py
@@ -3,23 +3,59 @@ from rest_framework.generics import ListAPIView , RetrieveAPIView
 from A.models import(
     Parent_A ,
     Activites_A ,
-    Activites_student_teacher_A ,
     Course_A ,
     Classroom_A ,
     Student_A ,
-    Student_Course_A ,
-    Sudent_Teacher_A ,
     Teacher_A
 )
 
 from .serializers import(
     ParentSerializer ,
+    ActivitesSerializer ,
+    ClassroomSerializer ,
+    CourseSerializer ,
+    StudentSerializer ,
+    TeacherSerializer
 )
 
 class ParentListAPIVeiw(ListAPIView):
     queryset = Parent_A.objects.all()
     serializer_class = ParentSerializer
-
 class ParentRetrieveAPIView(RetrieveAPIView):
     queryset = Parent_A.objects.all()
     serializer_class = ParentSerializer
+
+class ActivitesListAPIVeiw(ListAPIView):
+    queryset = Activites_A.objects.all()
+    serializer_class = ActivitesSerializer
+class ActivitesRetrieveAPIView(RetrieveAPIView):
+    queryset = Activites_A.objects.all()
+    serializer_class = ActivitesSerializer
+
+class ClassroomListAPIVeiw(ListAPIView):
+    queryset = Classroom_A.objects.all()
+    serializer_class = ClassroomSerializer
+class ClassroomRetrieveAPIView(RetrieveAPIView):
+    queryset = Classroom_A.objects.all()
+    serializer_class = ClassroomSerializer
+
+class CoursesListAPIVeiw(ListAPIView):
+    queryset = Course_A.objects.all()
+    serializer_class = CourseSerializer
+class CoursesRetrieveAPIView(RetrieveAPIView):
+    queryset = Course_A.objects.all()
+    serializer_class = CourseSerializer
+
+class StudentListAPIVeiw(ListAPIView):
+    queryset = Student_A.objects.all()
+    serializer_class = StudentSerializer
+class StudentRetrieveAPIView(RetrieveAPIView):
+    queryset = Student_A.objects.all()
+    serializer_class = StudentSerializer
+
+class TeacherListAPIVeiw(ListAPIView):
+    queryset = Teacher_A.objects.all()
+    serializer_class = TeacherSerializer
+class TeacherRetrieveAPIView(RetrieveAPIView):
+    queryset = Teacher_A.objects.all()
+    serializer_class = TeacherSerializer
